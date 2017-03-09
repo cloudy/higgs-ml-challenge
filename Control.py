2a73f6ee6e47e89bbcbffee4d19d4999fef85250
binarysaurus <im@joe.cloud>
2017-03-08 20:39:09 -0600

    refactored Control

--- Control.py
+++ Control.py
@@ -2,41 +2,28 @@
 import ProcessFile as pf
 import PlotData as pld
 
-# Loading and cleaning data
 
 filename = "/data/higgsml/training.csv"
 
-dataframe = pf.LoadFile(filename)
+def graphdata(dataframe):
+	datasignal = pf.ProcessFrame(dataframe, dataframe.Label, 1)
+	databack = pf.ProcessFrame(dataframe, dataframe.Label, 0)
+	figs = [pld.PlotData([datasignal, databack], var) for var in pf.GetVariables(datasignal)]
+	pld.SavePlots(figs)
+	pld.ClosePlots(figs)
 
-dataframe = pf.ReplaceValues(dataframe)
+def trainwdata(dataframe):
+	train_df, test_df = pf.SplitData(dataframe)
+	print len(train_df), len(test_df)
+	xtrain = pf.ProcessFrame(train_df, train_df.Label, 1)
+	ytrain = train_df.Label
+	print ytrain
 
-train_df, test_df = pf.SplitData(dataframe)
+def main():
+	dataframe = pf.LoadFile(filename)
+	dataframe = pf.ReplaceValues(dataframe)
+	graphdata(dataframe)
+	trainwdata(dataframe)
 
-print len(train_df), len(test_df)
-
-xtrain = pf.ProcessFrame(train_df, train_df.Label, 1)
-ytrain = train_df.Label
-
-
-print ytrain
-
-
-datasignal = pf.ProcessFrame(dataframe, dataframe.Label, 1)
-databack = pf.ProcessFrame(dataframe, dataframe.Label, 0)
-
-# Generating/saving plots
-
-figs = [pld.PlotData([datasignal, databack], var) for var in pf.GetVariables(datasignal)]
-pld.SavePlots(figs)
-pld.ClosePlots(figs)
-
-# Training
-
-
-
-# Testing model
-
-
-
-# Saving model
-
+if __name__ == "__main__":
+	main()
